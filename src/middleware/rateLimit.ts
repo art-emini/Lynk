026d25017da8183919f78fd6c56d25ea580386d4
ksplatdev <aviatorbuzz921@gmail.com>
2021-08-18 16:53:29 -0400

    Added firebase, changed ratelimit middleware

--- src/middleware/rateLimit.ts
+++ src/middleware/rateLimit.ts
@@ -4,61 +4,61 @@ export const authLimiter = rateLimit({
 	windowMs: 1000 * 60 * 15,
 	max: 7,
 	message:
-		'You have reached the max amount of auth calls in 15 minutes. Please try again later.',
+		'You have reached the max amount of auth calls. Please try again later.',
 });
 
 export const createLynkLimiter = rateLimit({
-	windowMs: 1000 * 60 * 15,
-	max: 10,
+	windowMs: 1000 * 60 * 5,
+	max: 5,
 	message:
-		'You have reached the max amount of createLynk calls in 15 minutes. Please try again later.',
+		'You have reached the max amount of createLynk calls. Please try again later.',
 });
 
 export const deleteLynkLimiter = rateLimit({
 	windowMs: 1000 * 60 * 10,
 	max: 15,
 	message:
-		'You have reached the max amount of deleteLynk calls in 10 minutes. Please try again later.',
+		'You have reached the max amount of deleteLynk calls. Please try again later.',
 });
 
 export const deleteUserLimiter = rateLimit({
-	windowMs: 1000 * 60 * 15,
-	max: 2,
+	windowMs: 1000 * 60 * 5,
+	max: 1,
 	message:
-		'You have reached the max amount of deleteUser calls in 15 minutes. Please try again later.',
+		'You have reached the max amount of deleteUser calls. Please try again later.',
 });
 
 export const createUserLimiter = rateLimit({
-	windowMs: 1000 * 60 * 15,
+	windowMs: 1000 * 60 * 5,
 	max: 2,
 	message:
-		'You have reached the max amount of createUser calls in 15 minutes. Please try again later.',
+		'You have reached the max amount of createUser calls. Please try again later.',
 });
 
 export const lynkViewLimiter = rateLimit({
 	windowMs: 1000 * 60 * 15,
 	max: 10,
 	message:
-		'You have reached the max amount of lynkView calls in 15 minutes. Please try again later.',
+		'You have reached the max amount of lynkView calls. Please try again later.',
 });
 
 export const accountViewLimiter = rateLimit({
 	windowMs: 1000 * 60 * 10,
 	max: 20,
 	message:
-		'You have reached the max amount of account calls in 15 minutes. Please try again later.',
+		'You have reached the max amount of account calls. Please try again later.',
 });
 
 export const editLynkLimiter = rateLimit({
 	windowMs: 1000 * 60 * 10,
 	max: 10,
 	message:
-		'You have reached the max amount of editLynk calls in 10 minutes. Please try again later.',
+		'You have reached the max amount of editLynk calls. Please try again later.',
 });
 
 export const getStatsLimiter = rateLimit({
 	windowMs: 1000 * 60 * 15,
 	max: 100,
 	message:
-		'You have reached the max amount of getStats calls in 10 minutes. Please try again later.',
+		'You have reached the max amount of getStats calls. Please try again later.',
 });
